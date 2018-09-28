3d590172095897c7c98d4addcdb00ff30e393407
jcft1972 <33870215+jcft1972@users.noreply.github.com>
2018-09-28 12:12:10 -0400

    Mas sobre el path de java.

--- jenkins/scripts/deploy.bat
+++ jenkins/scripts/deploy.bat
@@ -1,4 +1,4 @@
 mkdir \destinoapp2
 mkdir \destinoapp2\exec
 copy target\my-app-1.0-SNAPSHOT.jar \destinoapp2\exec
-%JAVA_HOME%\bin\java -jar \destinoapp2\exec\my-app-1.0-SNAPSHOT.jar
+java -jar \destinoapp2\exec\my-app-1.0-SNAPSHOT.jar
